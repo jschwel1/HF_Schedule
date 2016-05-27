a411f00241dd9cce50a359cf032164ea0ab4b70e
jschwel1 <jschwel1@binghamton.edu>
2016-05-26 22:13:02 -0400

    Started GUI and it works for opening/saving trainee lists. Also added button to remove trainee from the list

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,10 @@ import javax.swing.JFileChooser;
 
 public class Main {
 	public static void main(String[] args) throws FileNotFoundException{
-		run();
+		Base b = new Base();
+		
+		
+//		run();
 		
 //		JFileChooser fc = new JFileChooser();
 //		File f = new File("");
@@ -133,4 +136,96 @@ public class Main {
 			System.out.println();
 		}
 	}
+	
+	public static void run(ArrayList<Trainee> tList, Shift[][] schedule) throws FileNotFoundException{
+		ArrayList<Trainee> list1 = tList;
+		ArrayList<Trainee> list2 = new ArrayList<Trainee>();
+		Shift[][] shift = schedule;
+		String error = "The following trainees did not get at least one shift: ";
+		
+		
+		// fill schedule with first-shifts
+		while (list1.size() > 0){
+			int day = list1.get(0).getPrefDay();
+			int time = list1.get(0).getPrefTime();
+			
+			
+			// make sure the trainee hasn't gone through all preferences:
+			if (list1.get(0).getIter() == Trainee.NUM_PREFERENCES){
+				error+=list1.get(0).getName();
+				error+=", ";
+				list1.remove(0);
+				continue;
+			}
+			
+			// check if the first trainee in the queue's top choice is taken
+			if (shift[day][time].hasTrainee()){
+				// if no preceptors on shift, go to priority
+				if (!shift[day][time].hasCCPrec() && !shift[day][time].hasDrPrec()){
+					if (shift[day][time].getTrainee().hasHigherPriorityThan(list1.get(0))){
+						list1.get(0).nextChoice();
+						continue;
+					}
+					else{
+						list1.add(shift[day][time].getTrainee());
+						shift[day][time].setTrainee(list1.get(0));
+						
+					}
+				}
+				// check that the correct preceptor is on shift
+				else if (((shift[day][time].hasCCPrec() && shift[day][time].getTrainee().isCCPrecepting())
+							|| (shift[day][time].hasDrPrec() && shift[day][time].getTrainee().isDrPrecepting()))
+						&& !((list1.get(0).isCCPrecepting() && shift[day][time].hasCCPrec()) 
+							|| (list1.get(0).isDrPrecepting() && shift[day][time].hasDrPrec()))){
+					list1.get(0).nextChoice();
+					continue;
+				}
+				else if (((shift[day][time].hasCCPrec() && shift[day][time].getTrainee().isCCPrecepting())
+						|| (shift[day][time].hasDrPrec() && shift[day][time].getTrainee().isDrPrecepting()))
+					&& !((list1.get(0).isCCPrecepting() && shift[day][time].hasCCPrec()) 
+						|| (list1.get(0).isDrPrecepting() && shift[day][time].hasDrPrec()))){
+					list1.add(shift[day][time].getTrainee());
+					shift[day][time].setTrainee(list1.get(0));
+				}
+				// neither will be precepting what they want, go to priority
+				else{
+					if (shift[day][time].getTrainee().hasHigherPriorityThan(list1.get(0))){
+						list1.get(0).nextChoice();
+						continue;
+					}
+					else{
+						list1.add(shift[day][time].getTrainee());
+						shift[day][time].setTrainee(list1.get(0));
+						list1.remove(0);
+						continue;
+					}
+				}
+				
+			}
+			else{
+				shift[day][time].setTrainee(list1.get(0));
+				
+			}
+			list1.get(0).nextChoice();
+			list2.add(list1.get(0));
+			list1.remove(0);
+		}
+		
+		// fill schedule with second-shifts (Pretty much identical to the first loop
+	/*	while (list2.size() > 0){
+			
+		}
+	*/	
+		/* Print out schedule graphically and/or in excel/.csv file */
+		
+		
+		for (int d = 0; d < 7; d++){
+			for (int t = 0; t < 8; t++){
+				System.out.print(shift[d][t].getTrainee().getName() + "\t");
+			}
+			System.out.println();
+		}
+	}
+
 }
+
--- src/Trainee.java
+++ src/Trainee.java
@@ -1,8 +1,18 @@
- import java.io.File;
+ import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Trainee {
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+public class Trainee implements ActionListener{
 	public static final int NUM_PREFERENCES = 6;
 	public static final int SHIFTS_PER_DAY = 7;
 	int prefDay[];
@@ -12,12 +22,20 @@ public class Trainee {
 	boolean ccPrec;
 	boolean drPrec;
 	String name;
-	
+
+	// for GUI Builder
+	JButton shiftButton[][];
+	JButton submit;
+	JTextField nameInput;
+	JTextField semesterInput;
+	JCheckBox ccPrecepting;
+	JCheckBox drPrecepting;
 	
 	Trainee(){
 		prefDay = new int[NUM_PREFERENCES];
 		prefTime = new int[NUM_PREFERENCES];
 		iterator = 0;
+		
 	}
 	
 	Trainee(Scanner s) throws FileNotFoundException{
@@ -33,6 +51,7 @@ public class Trainee {
 		while (count < NUM_PREFERENCES){
 
 			// get preference day
+
 			input = s.nextInt();
 		//	while (input >= 7) input = s.nextInt();
 			prefDay[count] = input;
@@ -61,6 +80,8 @@ public class Trainee {
 //		System.out.println(drPrec?"is ":"is not " + "Driver Precepting");
 //		System.out.println("Has been in for " + semesters + " Semesters");
 	}
+	
+
 	
 	public void fillPrefs(){
 		Scanner s = new Scanner(System.in);
@@ -143,6 +164,21 @@ public class Trainee {
 		String s = "";
 		s += name + "\n";
 		for (int i = 0; i < NUM_PREFERENCES; i++){
+			s+= prefDay[i] + "," + prefTime[i]+',';
+		}
+		if (ccPrec) s+= "y,";
+		else s+="n,";
+		
+		if (drPrec) s+= "y,";
+		else s+="n,";
+		
+		s+=semesters;
+		
+		return s;
+	}
+	public String getInfo(){
+		String s = "";
+		for (int i = 0; i < NUM_PREFERENCES; i++){
 			s+= prefDay[i] + "\t" + prefTime[i]+'\t';
 		}
 		if (ccPrec) s+= "y\t";
@@ -151,9 +187,156 @@ public class Trainee {
 		if (drPrec) s+= "y\t";
 		else s+="n\t";
 		
-		s+=semesters+"\n";
+		s+=semesters;
 		
 		return s;
 	}
+
+	public void GUIBuild(){
+		JFrame frame = new JFrame("New Trainee");
+		Container window = frame.getContentPane();
+		nameInput = new JTextField("Name");
+		semesterInput = new JTextField("# semesters");
+		ccPrecepting = new JCheckBox("CC Precepting");
+		drPrecepting = new JCheckBox("Driver Precepting");
+		submit = new JButton("Submit");
+		
+		
+		shiftButton = new JButton[8][SHIFTS_PER_DAY+1];
+		
+		
+		submit.addActionListener(this);
+		submit.setActionCommand("submit");
+		
+		iterator = 0;
+		for (int d = 1; d < shiftButton.length; d++){
+			for (int t = 1; t < shiftButton[0].length; t++){
+				shiftButton[d][t] = new JButton("#");
+				shiftButton[d][t].addActionListener(this);
+				shiftButton[d][t].setActionCommand("pref"+d+","+t);
+			}
+		}
+		
+		shiftButton[0][0] = new JButton("Time\\Day");
+		shiftButton[1][0] = new JButton("Sunday");
+		shiftButton[2][0] = new JButton("Monday");
+		shiftButton[3][0] = new JButton("Tuesday");
+		shiftButton[4][0] = new JButton("Wednesday");
+		shiftButton[5][0] = new JButton("Thursday");
+		shiftButton[6][0] = new JButton("Friday");
+		shiftButton[7][0] = new JButton("Saturday");
+		
+		shiftButton[0][1] = new JButton("0000");
+		shiftButton[0][2] = new JButton("0600");
+		shiftButton[0][3] = new JButton("0900");
+		shiftButton[0][4] = new JButton("1200");
+		shiftButton[0][5] = new JButton("1500");
+		shiftButton[0][6] = new JButton("1800");
+		shiftButton[0][7] = new JButton("2100");
+		
+		frame.setVisible(true);
+		frame.setVisible(true);
+		frame.setSize(800,600);
+		frame.setLocationRelativeTo(null);
+		window.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		
+		c.gridx = 0;
+		c.gridy = 0;
+		c.gridwidth = 4;
+		window.add(nameInput, c);
+		
+		c.gridx = 4;
+		c.gridy = 0;
+		c.gridwidth = 1;
+		window.add(semesterInput, c);
+		
+		c.gridx = 5;
+		c.gridy = 0;
+		c.gridwidth = 1;
+		window.add(ccPrecepting, c);
+		
+		c.gridx = 6;
+		c.gridy = 0;
+		c.gridwidth = 1;
+		window.add(drPrecepting, c);
+		
+		c.gridx = 7;
+		c.gridy = 0;
+		c.gridwidth = 1;
+		window.add(submit, c);
+		
+		
+		
+		for (int d = 0; d < shiftButton.length; d++){
+			for (int t = 0; t < shiftButton[0].length; t++){
+				c.gridx=d;
+				c.gridy=t+1;
+				window.add(shiftButton[d][t], c);
+			}
+		}
+		
+		
+		
+		//while (iterator < NUM_PREFERENCES){}
+		//iterator = 0;
+		
+		// add information
+		
+	}
+	
+	
+	
+	public void actionPerformed(ActionEvent e) {
+		for (int d = 1; d < shiftButton.length; d++){
+			for (int t = 1; t < shiftButton[0].length; t++){
+				if (e.getActionCommand().equals("pref"+d+","+t)){
+					
+					if(shiftButton[d][t].getText().equals("#")){
+						if (iterator >= NUM_PREFERENCES) return;
+						shiftButton[d][t].setText(iterator+1+"");
+						iterator++;
+					}
+					else{
+						for (int dd = 1; dd < shiftButton.length; dd++){
+							for (int tt = 1; tt < shiftButton[0].length; tt++){
+								if(!shiftButton[dd][tt].getText().equals("#")){
+									if(Integer.parseInt(shiftButton[dd][tt].getText()) > Integer.parseInt(shiftButton[d][t].getText())){
+										shiftButton[dd][tt].setText((Integer.parseInt(shiftButton[dd][tt].getText())-1)+"");
+									}
+								}
+							}
+						}
+						
+						shiftButton[d][t].setText("#");
+						iterator--;
+					}
+				}
+			}
+		}
+		
+		if (e.getSource() == submit){
+			try {
+				name = nameInput.getText();
+				semesters = Integer.parseInt(semesterInput.getText());
+				ccPrec = ccPrecepting.isSelected();
+				drPrec = drPrecepting.isSelected();
+				
+				for (int d = 1; d < shiftButton.length; d++){
+					for (int t = 1; t < shiftButton[0].length; t++){
+						if (!shiftButton[d][t].getText().equals("#")){
+							prefDay[Integer.parseInt(shiftButton[d][t].getText())-1] = d-1;
+							prefTime[Integer.parseInt(shiftButton[d][t].getText())-1] = t-1;
+						}
+					}
+				}
+			} catch (NumberFormatException nfe){
+				JOptionPane.showMessageDialog(null, "Error: check input types. Probably caused by # semesters field, ensure it is entered as a number");
+			}
+		}
+	}
+	
+	
 	
 }
